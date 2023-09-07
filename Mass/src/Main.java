9c3f79f0b557098e10520149d3f9f70f3ace24ee
iliasen <iliasen03@mail.ru>
2023-09-07 18:42:06 +0300

    Complite task mergeArrays in 2 ways

--- Mass/src/Main.java
+++ Mass/src/Main.java
@@ -1,9 +1,22 @@
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        /*
         System.out.println("Hello and welcome!");
         mass(5);
 
         isPalindrome("Madam, I'm Adam!");
+
+        factorial(6);
+*/
+        int[] mass1 = {0,2,2,5,7,10,22,74,75,77,78,90};
+        int[] mass2 = {1,3,12,14,110};
+
+        System.out.println("_______________");
+        mergeArrays(mass1, mass2);
     }
 
 
@@ -30,5 +43,59 @@ public class Main {
         }
         System.out.println("Ok");
         return true;
+    }
+
+    public static BigInteger factorial(int value) {
+        BigInteger result = BigInteger.valueOf(1);
+        for(int i = 1; i <= value; i++){
+            BigInteger x = BigInteger.valueOf(i);
+            result = result.multiply(x);
+            System.out.println(result);
+        }
+        return result; // your implementation here
+    }
+
+    public static int[] mergeArrays(int[] a1, int[] a2) {
+        int[] arr = new int[a1.length + a2.length];
+
+        int i=0,j=0,k=0;
+        while(i < a1.length && j < a2.length){
+            if(a1[i]<=a2[j]){
+                arr[k]=a1[i];
+                i++;
+            }else{
+                arr[k]=a2[j];
+                j++;
+            }
+            k++;
+        }
+        if(i == a1.length-1){
+            arr[k]=a1[i];
+            k++;
+            while (j < a2.length){
+                arr[k]=a2[j];
+                j++;
+                k++;
+            }
+        }else{
+            arr[k]=a2[j];
+            k++;
+            while (i < a1.length){
+                arr[k]=a1[i];
+                i++;
+                k++;
+            }
+        }
+        //System.out.println(Arrays.toString(arr));
+        return arr;
+
+        /*int [] result = new int[a1.length + a2.length];
+        int pos1 = 0;
+        int pos2 = 0;
+        while(pos1<a1.length || pos2<a2.length) {
+            result[pos1+pos2] = (pos1<a1.length && (pos2 == a2.length || a1[pos1]<a2[pos2]) ?
+                    a1[pos1++] : a2[pos2++]);
+        }
+        return result;*/
     }
 }
